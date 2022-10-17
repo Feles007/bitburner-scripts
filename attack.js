318b8213fee1a7b6de5507f45d47492592afd68b
Feles007 <75643325+Feles007@users.noreply.github.com>
2022-10-16 18:59:06 -0700

    Create attack.js

---
+++ attack.js
@@ -0,0 +1,17 @@
+export async function main(ns) {
+
+	const target = "";
+  const total_ram = 1024;
+
+	const hack_js_ram = ns.getScriptRam("hack.js");
+	const max_ram = total_ram - ns.getScriptRam("attack.js");
+	const threads = Math.floor(max_ram / hack_js_ram);
+
+	ns.run(
+		"hack.js",
+		threads,
+		target,
+		ns.getServerMaxMoney(target) * 0.3,
+		ns.getServerMinSecurityLevel(target) * 3.5,
+	);
+}
